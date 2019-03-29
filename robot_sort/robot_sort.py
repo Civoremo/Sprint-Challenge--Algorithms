0388a89d2bff7d430ed842ccb313db4e0660f91d
Civoremo <omerovic_n@hotmail.com>
2019-03-29 13:05:08 -0400

    robot_sort small test passed

--- robot_sort/robot_sort.py
+++ robot_sort/robot_sort.py
@@ -81,11 +81,13 @@ class SortingRobot:
         Turn on the robot's light
         """
         self._light = "ON"
+
     def set_light_off(self):
         """
         Turn off the robot's light
         """
         self._light = "OFF"
+
     def light_is_on(self):
         """
         Returns True if the robot's light is on and False otherwise.
@@ -97,14 +99,61 @@ class SortingRobot:
         Sort the robot's list.
         """
         # Fill this out
-        pass
+        # selection sort
+        # pick up first item
+        # move right and compare
+        # if pickedUp item is less then currentPos item; move right
+        # if pickedUp item is greater than currentPos item: swapItems
+        # repeat process until you reach the end of list
+        # bring item back to position with value None
+        # go to next position and pick up item
+        # move right comparing with those items
+        # if pickedUp item is less than currentPos item; move right and compare again
+        # if pickedUp item is greater than currentPos item: swap items
+        # keep going comparing items until yo reach the end of list
+        # bring back item to position with value None
+        #
+        # repeat those step until pickedUp item is at end of list
+
+        self.swap_item()
+        # print(f'first item: {self._item}')
+        self.set_light_on()
+        while self.light_is_on():
+            while self.can_move_right():
+                self.move_right()
+                # print(f'moved right: {self._position}')
+                if self.compare_item():
+                    # print(f'swapping item: {self._item}')
+                    self.swap_item()
+                    # print(
+                    #     f'item is smaller than what we have, new item:{self._item}')
+
+            while self.compare_item() is not None:
+                self.move_left()
+                # print(f'moving left: {self._position}')
+                if self.compare_item() == None:
+                    # print(f'old position item: {self._item}')
+                    self.swap_item()
+                    # print(
+                    #     f'item is brought back to NONE position: {self._position} item: {self._item}')
+
+            if self.can_move_right() is not False:
+                # print(f'item returned, move over again and search again')
+                self.move_right()
+                if self.can_move_right():
+                    self.swap_item()
+                else:
+                    self.set_light_off()
+
+                # pass
 
 
 if __name__ == "__main__":
     # Test our your implementation from the command line
     # with `python robot_sort.py`
 
-    l = [15, 41, 58, 49, 26, 4, 28, 8, 61, 60, 65, 21, 78, 14, 35, 90, 54, 5, 0, 87, 82, 96, 43, 92, 62, 97, 69, 94, 99, 93, 76, 47, 2, 88, 51, 40, 95, 6, 23, 81, 30, 19, 25, 91, 18, 68, 71, 9, 66, 1, 45, 33, 3, 72, 16, 85, 27, 59, 64, 39, 32, 24, 38, 84, 44, 80, 11, 73, 42, 20, 10, 29, 22, 98, 17, 48, 52, 67, 53, 74, 77, 37, 63, 31, 7, 75, 36, 89, 70, 34, 79, 83, 13, 57, 86, 12, 56, 50, 55, 46]
+    l = [15, 41, 58, 49, 26, 4, 28, 8, 61, 60, 65, 21, 78, 14, 35, 90, 54, 5, 0, 87, 82, 96, 43, 92, 62, 97, 69, 94, 99, 93, 76, 47, 2, 88, 51, 40, 95, 6, 23, 81, 30, 19, 25, 91, 18, 68, 71, 9, 66, 1,
+         45, 33, 3, 72, 16, 85, 27, 59, 64, 39, 32, 24, 38, 84, 44, 80, 11, 73, 42, 20, 10, 29, 22, 98, 17, 48, 52, 67, 53, 74, 77, 37, 63, 31, 7, 75, 36, 89, 70, 34, 79, 83, 13, 57, 86, 12, 56, 50, 55, 46]
 
     robot = SortingRobot(l)
 
